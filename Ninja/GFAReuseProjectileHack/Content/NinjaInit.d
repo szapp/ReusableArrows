2cc06b79b46ec571b3628b9a3b019fb13219f060
szapp <dev.szapp@gmail.com>
2020-01-24 19:58:31 +0100

    Add warning message

--- Ninja/GFAReuseProjectileHack/Content/NinjaInit.d
+++ Ninja/GFAReuseProjectileHack/Content/NinjaInit.d
@@ -4,6 +4,7 @@
 func void Ninja_GFAReuseProjectileHack_Init() {
     MEM_InitAll();
 
+    // Do not use this patch as a template: It is not well formatted (presupposes variables) and poses a bad example
     if (!(GFA_Flags & GFA_REUSE_PROJECTILES)) {
         RemoveHookF(oCAIArrow__DoAI_rtn, 6, GFA_CC_FadeProjectileVisibility); // Remove fading from Gothic 1
 
