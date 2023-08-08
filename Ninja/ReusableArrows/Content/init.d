7578bb10f79e52c095cda950a813637a02f2d538
szapp <dev.szapp@gmail.com>
2023-08-08 16:41:59 +0200

    Make compatible with 1.12f and 1.30

--- Ninja/ReusableArrows/Content/init.d
+++ Ninja/ReusableArrows/Content/init.d
@@ -13,19 +13,19 @@ func void Ninja_ReusableArrows_Menu() {
     // First, check for the necessary symbols of GFA to prevent parser errors
     if (!MEM_GetSymbol("GFA_Init"))
     || (!MEM_GetSymbol("GFA_REUSE_PROJECTILES")) || (!MEM_GetSymbol("GFA_CUSTOM_COLLISIONS")) {
-        MEM_SendToSpy(zERR_TYPE_FATAL, "Reusable Arrows: Gothic Free Aim (GFA) not installed or invalid version");
+        MEM_SendToSpy(zERR_TYPE_FATAL, "Reusable Arrows: Gothic Free Aim [GFA] not installed or invalid version.");
         return;
     };
 
     // Hook the initialization function
-    HookDaedalusFuncS("GFA_Init", "Ninja_ReusableArrows_Hook");
+    HookDaedalusFuncS("GFA_Init", "Patch_ReusableArrows_Hook");
 };
 
 
 /*
  * Intercept the initialization of GFA and squeeze in the reusable projectile feature flag
  */
-func void Ninja_ReusableArrows_Hook(var int flags) {
+func void Patch_ReusableArrows_Hook(var int flags) {
     const int GFA_REUSE_PROJECTILES = 0;
     const int GFA_CUSTOM_COLLISIONS = 0;
     GFA_REUSE_PROJECTILES = MEM_ReadInt(MEM_GetSymbol("GFA_REUSE_PROJECTILES") + zCParSymbol_content_offset);
